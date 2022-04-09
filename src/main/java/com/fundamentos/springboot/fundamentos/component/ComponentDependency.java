fa03c5b95faa629fe94a05617b45a131abe07464
gualberto <elianosft82@gmail.com>
2022-04-08 23:25:03 -0500

    Primeros archivos

---
+++ src/main/java/com/fundamentos/springboot/fundamentos/component/ComponentDependency.java
@@ -0,0 +1,5 @@
+package com.fundamentos.springboot.fundamentos.component;
+
+public class ComponentDependency {
+    void saludar();
+}
---
+++ src/main/java/com/fundamentos/springboot/fundamentos/component/ComponentImplement.java
@@ -0,0 +1,14 @@
+package com.fundamentos.springboot.fundamentos.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class ComponentImplement extends ComponentDependency {
+    @Override
+    public void saludar(){
+
+    }
+
+
+}
